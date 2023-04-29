7b99a1e8417eca33605d0205e560ae259d9ce702
Stanislav Kintoriak <kintoriak@gmail.com>
2023-04-29 23:00:18 +0300

    commit 3
    1) installed prop-types;
    2) Profile.js module added;
    3) Added Profile.js to App.js;
    4) Added json data files for all future modules.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Profile } from '../components/Profile/Profile.js';
+
+import user from '../data/user.json';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        imageUrl={user.avatar}
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+    </>
   );
 };
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
-
+import PropTypes from 'prop-types';
+import styles from '../Profile/Profile.module.css'
 
 export const Profile = ({
     imageUrl,
@@ -8,31 +9,43 @@ export const Profile = ({
     stats: { followers, views, likes },
   }) => {
     return (
-        <div class="profile">
-  <div class="description">
+        <div class={styles.profile}>
+  <div class={styles.description}>
     <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
+      src={imageUrl}
+      alt={name}
+      class={styles.avatar}
     />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
+    <p class={styles.name}>{name}</p>
+    <p class={styles.tag}>{tag}</p>
+    <p class={styles.location}>{location}</p>
   </div>
 
-  <ul class="stats">
+  <ul class={styles.stats}>
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
+      <span class={styles.label}>Followers</span>
+      <span class={styles.quantity}>{followers}</span>
     </li>
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
+      <span class={styles.label}>Views</span>
+      <span class={styles.quantity}>{views}</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
+      <span class={styles.label}>Likes</span>
+      <span class={styles.quantity}>{likes}</span>
     </li>
   </ul>
 </div>
     )}
+
+    Profile.propTypes = {
+        imageUrl:PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        tag: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
+        stats: PropTypes.exact({
+            followers: PropTypes.number.isRequired,
+            views: PropTypes.number.isRequired,
+            likes: PropTypes.number.isRequired,
+        }).isRequired
+    }
