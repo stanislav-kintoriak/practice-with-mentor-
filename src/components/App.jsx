bd4b9cb3fbc7a2b43fe721aa5d810705aaa3acd6
Stanislav Kintoriak <kintoriak@gmail.com>
2023-04-30 12:30:18 +0300

    commit 5
    1) styles for profile;
    2) commented all others modules.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
 import { Profile } from '../components/Profile/Profile.js';
-import { Statistics } from '../components/Statistics/Statistics.js';
-import { FriendList } from './FriendList/FriendList.js';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory.js'
-
+// import { Statistics } from '../components/Statistics/Statistics.js';
+// import { FriendList } from '../components/FriendList/FriendList.js';
+// import { TransactionHistory } from '../components/TransactionHistory/TransactionHistory.js';
 import user from '../data/user.json';
-import data from '../data/data.json'
-import friends from '../data/friends.json'
-import transactions from '../data/transactions.json'
-
+// import data from '../data/data.json';
+// import friends from '../data/friends.json';
+// import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -19,14 +17,9 @@ export const App = () => {
         location={user.location}
         stats={user.stats}
       />
-      <Statistics
-      title="UPLOAD STATS"
-      stats={data}
-      />
-      <FriendList
-      friends={friends}
-      />
-      <TransactionHistory items={transactions} />;
+      {/* <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
     </>
   );
 };
--- src/components/FriendList/FriendListItem/FriendListItem.js
+++ src/components/FriendList/FriendListItem/FriendListItem.js
@@ -1,25 +1,25 @@
-import PropTypes from 'prop-types';
-import styles from './FriendListItem.module.css';
+// import PropTypes from 'prop-types';
+// import styles from './FriendListItem.module.css';
 
-export const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li class={styles.item}>
-      <span
-        class={`${styles.status} ${isOnline} ? styles.true: styles:false`}
-      ></span>
-      <img
-        class={styles.avatar}
-        src={avatar}
-        alt={`${name} picture`}
-        width="48"
-      />
-      <p class={styles.name}>{name}</p>
-    </li>
-  );
-};
+// export const FriendListItem = ({ avatar, name, isOnline }) => {
+//   return (
+//     <li class={styles.item}>
+//       <span
+//         class={`${styles.status} ${isOnline} ? styles.true: styles:false`}
+//       ></span>
+//       <img
+//         class={styles.avatar}
+//         src={avatar}
+//         alt={`${name} picture`}
+//         width="48"
+//       />
+//       <p class={styles.name}>{name}</p>
+//     </li>
+//   );
+// };
 
-FriendListItem.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-};
+// FriendListItem.propTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   isOnline: PropTypes.bool.isRequired,
+// };
